d4891d03bbd946e02fe764f9c084c0dd8f48f6a2
Student <Student@LabVM2047644.fnlvkk4yo3wetfw0y1pmd3tfyb.jnbx.internal.cloudapp.net>
2026-04-04 19:06:23 +0200

    Updated project

--- src/part_1/PART_1.java
+++ src/part_1/PART_1.java
@@ -4,6 +4,7 @@
  */
 package part_1;
 
+import java.util.Scanner;
 /**
  *
  * @author Student
@@ -14,7 +15,21 @@ public class PART_1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+    Scanner input = new Scanner(System.in);
+    
+    System.out.println("Please enter a username (a maximum of 5 characters and include an underscore): ");
+    //Input the  username
+    String userName = input.nextLine();
+     //Condition testing using BOOLEAN
+     if(userName.contains("_") && userName.length() <= 5){
+         System.out.println ("Captured successfully");
+     }else{
+         System.out.println("Username invalid, ensure there is an underscore and a maximum of 5 characters");
+         
+         
+         System.out.println("Please enter a Pass");
+     }
+     input.close();
     }
     
 }
